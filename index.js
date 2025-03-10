deb9cf13678a4449e3004270b5873480d113179a
artyomagadzhanyan421 <artyom.agadzhanyan421@gmail.com>
2025-03-10 22:41:56 +0500

    vars commit

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
+const dotenv = require('dotenv');
 const cors = require('cors');
 const connectDB = require('./mongodb/connect');
-const dotenv = require('dotenv');
 const userRoutes = require('./routes/userRoutes');
 
 dotenv.config();
